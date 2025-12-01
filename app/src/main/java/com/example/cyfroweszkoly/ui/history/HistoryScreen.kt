32cfbc9e38920d2e4052a97c4cbd2302307eaa65
szymonh <szymon.haponik@cyfroweszkoly.pl>
2025-12-01 14:11:54 +0100

    create preview for history screen

--- app/src/main/java/com/example/cyfroweszkoly/ui/history/HistoryScreen.kt
+++ app/src/main/java/com/example/cyfroweszkoly/ui/history/HistoryScreen.kt
@@ -1,9 +1,17 @@
 package com.example.cyfroweszkoly.ui.history
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.cyfroweszkoly.navigation.AppNavHost
+import com.example.cyfroweszkoly.ui.theme.CyfroweSzkolyTheme
+
 
 
 @Composable
@@ -12,6 +20,17 @@ fun HistoryScreen(navController: NavController){
 
     ) {
         Text(text="Ekran historii szkoły")
+
+    }
+}
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun HistoryScreenPreview(){
+    val navController = rememberNavController()
+    CyfroweSzkolyTheme {
+        HistoryScreen(navController)
     }
 }
 
