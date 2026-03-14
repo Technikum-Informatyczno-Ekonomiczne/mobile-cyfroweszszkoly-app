6251c0fe6371a35415d0f2cb102c584bc7fb4ad9
Szymon <szymon.haponik@cyfroweszkoly.pl>
2026-03-14 18:46:10 +0100

    Add picture for liceum button route

--- app/src/main/java/com/example/cyfroweszkoly/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/cyfroweszkoly/ui/home/HomeScreen.kt
@@ -1,16 +1,29 @@
 package com.example.cyfroweszkoly.ui.history
 
+import android.media.Image
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.cyfroweszkoly.R
 import com.example.cyfroweszkoly.navigation.Screen
 import com.example.cyfroweszkoly.ui.theme.CyfroweSzkolyTheme
 
@@ -25,9 +38,16 @@ fun HomeScreen(navController: NavController){
         Button(onClick = { navController.navigate(route = Screen.Primary.route)}) {
             Text(text = "Szkoła podstawowa")
         }
-        Button(onClick = {navController.navigate(route = Screen.High.route )}){
-            Text(text = "Liceum IX")
-        }
+        Image(
+            painter = painterResource(R.drawable.liceum_button),
+            contentDescription = "Liceum IX",
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .fillMaxWidth(0.7f)
+                .aspectRatio(1.8f)   // dopasuj do proporcji zdjęcia
+                .clip(RoundedCornerShape(40.dp))
+                .clickable { navController.navigate(Screen.High.route) }
+        )
         Button(onClick = { navController.navigate(route = Screen.Tech.route )}) {
             Text(text = "Technikum XI")
         }
