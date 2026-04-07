4f023df47b41c40edbe9cd2394ad16a84d5fb09c
jarek m5 <jaroslaw.strzelecki@protonmail.com>
2026-04-07 11:34:43 +0200

    Add Preview for DrawerContent

--- app/src/main/java/com/example/cyfroweszkoly/ui/main/DrawerContent.kt
+++ app/src/main/java/com/example/cyfroweszkoly/ui/main/DrawerContent.kt
@@ -8,6 +8,8 @@ import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
 import androidx.compose.ui.unit.dp
 import com.example.cyfroweszkoly.navigation.Screen
 import kotlinx.coroutines.selects.select
@@ -41,4 +43,13 @@ fun DrawerContent(onNavigate: (String)->Unit){
         )
     }
 
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DrawerContentPreview(){
+    MaterialTheme() {
+        DrawerContent(onNavigate = {route -> })
+
+    }
 }
