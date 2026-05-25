d28eb8a6d63558a335d72b1b655cd6d42c116269
jarek m5 <jaroslaw.strzelecki@protonmail.com>
2026-05-25 13:44:20 +0200

    :)

--- app/src/main/java/com/example/cyfroweszkoly/data/model/ScheduleItem.kt
+++ app/src/main/java/com/example/cyfroweszkoly/data/model/ScheduleItem.kt
@@ -1,3 +1,14 @@
 package com.example.cyfroweszkoly.data.model
 
-data class ScheduleItem()
+import com.google.firebase.firestore.DocumentId
+
+data class ScheduleItem(
+    @DocumentId
+    val id: String = "", // Firebase automatycznie uzupełni to pole identyfikatorem dokumentu
+    val teacherName: String = "",
+    val className: String = "",
+    val location: String = "", // Nasza sala (w JSON: location)
+    val dayOfWeek: String = "",
+    val lessonNumber: Int = 0,
+    val time: String = ""
+)
--- app/src/main/java/com/example/cyfroweszkoly/viewmodel/ScheduleSearchViewModel.kt
+++ app/src/main/java/com/example/cyfroweszkoly/viewmodel/ScheduleSearchViewModel.kt
@@ -1,4 +1,42 @@
 package com.example.cyfroweszkoly.viewmodel
 
-class ScheduleSearchViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.cyfroweszkoly.data.model.ScheduleItem
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+
+enum class SearchType { TEACHER, ROOM, CLASS }
+
+class ScheduleSearchViewModel: ViewModel() {
+    private val db = FirebaseFirestore.getInstance()
+
+    private val _searchResults = MutableStateFlow<List<ScheduleItem>>(emptyList())
+    val serachResults = _searchResults.asStateFlow()
+
+    fun searchSchedule(query: String, type: SearchType){
+        if(query.isBlank()) return
+
+        val fieldName = when (type) {
+            SearchType.TEACHER -> "teacherName"
+            SearchType.ROOM -> "location"
+            SearchType.CLASS -> "className"
+        }
+
+
+        db.collection("schedules-mock")
+            .get()
+            .addOnSuccessListener { documents ->
+                val results = documents.map{ doc ->
+                    doc.toObject(ScheduleItem::class.java).copy(id = doc.id)
+
+                }
+                _searchResults.value =results
+            }
+            .addOnFailureListener {
+                _searchResults.value = emptyList()
+            }
+
+    }
 }
